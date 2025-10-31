2fc9fcda90d8f5bc743fde153d3c24110d8c4163
brinstussymethod <brianandrade20048@gmail.com>
2025-10-30 22:46:43 -0700

    Completed SDES Encryption and Decryption found in MainSDES.java file in SDES folder, and completed first SDES tasks. Next should be Triple SDES

--- SDES/MainSDES.java
+++ SDES/MainSDES.java
@@ -1,32 +1,78 @@
 package SDES;
 import tools.TenBitKey;
 import tools.IntToBit;
-import static SDES.SDES.leftShift;
-import static SDES.SDES.permuteMSB;
+
+import static SDES.SDES.*;
 
 
 public class MainSDES {
 
     public static void main(String[] args){
-        int key = 0b1010000010;
-        int[] P10 = {3,5,2,7,4,10,1,9,8,6};
-        int[] P8  = {6,3,7,4,8,5,10,9};
+         /* Implement SDES. To verify that your implementation of SDES is correct, try the following test cases:
+            TESTING
+            Raw Key           Plaintext             Ciphertext
 
-        int p10 = permuteMSB(key, P10, 10);
-        int L = (p10 >> 5) & 0x1F, R = p10 & 0x1F;
+            0000000000     10101010            00010001
 
-        System.out.println("P10: " + IntToBit.to10BitBinary(p10));           // 1000001100
-        L = leftShift(L,1); R = leftShift(R,1);
-        System.out.println("LS1: " + IntToBit.to5BitBinary(L) + " " + IntToBit.to5BitBinary(R)); // 00001 11000
-        int K1 = permuteMSB((L<<5)|R, P8, 10);
-        System.out.println("K1:  " + IntToBit.to8BitBinary(K1));             // 10100100
+            1110001110     10101010            11001010
 
-        L = leftShift(L,2); R = leftShift(R,2);
-        System.out.println("LS2: " + IntToBit.to5BitBinary(L) + " " + IntToBit.to5BitBinary(R)); // 00100 00011
-        int K2 = permuteMSB((L<<5)|R, P8, 10);
-        System.out.println("K2:  " + IntToBit.to8BitBinary(K2));
+            1110001110     01010101            01110000
 
-        // ADD THE SDES KEY ENCRYPTION SECOND PART 
+            1111111111     10101010            00000100
+        */
+        int answer = Encrypt(0b0000000000, 0b10101010);
+        System.out.println(IntToBit.to8BitBinary(answer));
+
+        int answer2 = Encrypt(0b1110001110, 0b10101010);
+        System.out.println(IntToBit.to8BitBinary(answer2));
+
+        int answer3 = Encrypt(0b1110001110, 0b01010101);
+        System.out.println(IntToBit.to8BitBinary(answer3));
+
+        int answer4 = Encrypt(0b1111111111, 0b10101010);
+        System.out.println(IntToBit.to8BitBinary(answer4));
+
+        int answerDecrypt = Decrypt(0b1111111111, 0b00000100);
+        System.out.println("Decrypted: " + IntToBit.to8BitBinary(answerDecrypt));
+        /*
+        Use your implementation to complete the following table:
+
+        Raw Key         Plaintext             Ciphertext
+
+        0000000000     00000000                 ?
+        1111111111     11111111                 ?
+        0000011111     00000000                 ?
+        0000011111     11111111                 ?
+        1000101110     ?                       00011100
+        1000101110     ?                       11000010
+        0010011111     ?                       10011101
+        0010011111     ?                       10010000
+         */
+        System.out.println("SDES Problems: ");
+        int c1 = Encrypt(0b0000000000, 0b00000000);
+        System.out.println(IntToBit.to8BitBinary(c1));   // 11110000
+
+        int c2 = Encrypt(0b1111111111, 0b11111111);
+        System.out.println(IntToBit.to8BitBinary(c2));   // 00001111
+
+        int c3 = Encrypt(0b0000011111, 0b00000000);
+        System.out.println(IntToBit.to8BitBinary(c3));   // 01000011
+
+        int c4 = Encrypt(0b0000011111, 0b11111111);
+        System.out.println(IntToBit.to8BitBinary(c4));   // 11100001
+
+        int p5 = Decrypt(0b1000101110, 0b00011100);
+        System.out.println(IntToBit.to8BitBinary(p5));   // 00111000
+
+        int p6 = Decrypt(0b1000101110, 0b11000010);
+        System.out.println(IntToBit.to8BitBinary(p6));   // 00001100
+
+        int p7 = Decrypt(0b0010011111, 0b10011101);
+        System.out.println(IntToBit.to8BitBinary(p7));   // 11111100
+
+        int p8 = Decrypt(0b0010011111, 0b10010000);
+        System.out.println(IntToBit.to8BitBinary(p8));   // 10100101
+
 
     }
 }
--- SDES/SDES.java
+++ SDES/SDES.java
@@ -48,6 +48,7 @@ public class SDES
         return ((value << s) | (value >>> (5 - s))) & 0x1F; 
     }
 
+
     public static int[] subkeys(int key10, int[] P10, int[] P8) {
         key10 &= 0x3FF; 
         int p10 = permuteMSB(key10, P10, 10);
@@ -63,8 +64,125 @@ public class SDES
 
         return new int[]{K1, K2}; 
     }
+    // Begin encryption algorithm
+    static final int[][] S0 = {
+            {1, 0, 3, 2},
+            {3, 2, 1, 0},
+            {0, 2, 1, 3},
+            {3, 1, 3, 2}
+    };
+    static final int[][] S1 = {
+            {0, 1, 2, 3},
+            {2, 0, 1, 3},
+            {3, 0, 1, 0},
+            {2, 1, 0, 3}
+    };
+
+    static final int[] EP = {4,1,2,3,2,3,4,1};
+    static final int[] P4 = {2,4,3,1};
+
+    static int getBit(int v, int posFromLeft, int inSize) {
+        int shift = inSize - posFromLeft;
+        return (v >> shift) & 1;
+    }
+    static int permute(int v, int inSize, int[] table) {
+        int m = table.length, r = 0;
+        for (int i = 0; i < m; i++) {
+            int b = getBit(v, table[i], inSize);
+            r |= b << (m - 1 - i);
+        }
+        return r;
+    }
+    static int sbox(int fourBits, int[][] box) {
+        int b1 = (fourBits >> 3) & 1;
+        int b2 = (fourBits >> 2) & 1;
+        int b3 = (fourBits >> 1) & 1;
+        int b4 = fourBits & 1;
+        int row = (b1 << 1) | b4;
+        int col = (b2 << 1) | b3;
+        return box[row][col] & 0b11;
+    }
+
+    static int F(int r4, int subkey8) {
+        int ep = permute(r4 & 0xF, 4, EP);
+        int x = ep ^ (subkey8 & 0xFF);
+        int left4 = (x >> 4) & 0xF;
+        int right4 = x & 0xF;
+        int s0 = sbox(left4, S0);
+        int s1 = sbox(right4, S1);
+        int out4 = ((s0 & 0x3) << 2) | (s1 & 0x3);
+        return permute(out4, 4, P4) & 0xF;
+    }
+
+    static int fK(int in8, int subkey8) {
+        int L = (in8 >> 4) & 0xF;
+        int R = in8 & 0xF;
+        int f = F(R, subkey8);
+        int Lp = L ^ f;
+        return ((Lp & 0xF) << 4) | (R & 0xF);
+    }
+
+    static int SW(int in8) {
+        int L = (in8 >> 4) & 0xF;
+        int R = in8 & 0xF;
+        return (R << 4) | L;
+    }
+
+
+    public static int Encrypt(int key, int plaintext) {
+        int[] P8Initial = {2, 6, 3, 1, 4, 8, 5, 7};
+        int[] P8Final = {4, 1, 3, 5, 7, 2, 8, 6};
+        int[] P10 = {3,5,2,7,4,10,1,9,8,6}; // for key
+        int[] P8  = {6,3,7,4,8,5,10,9}; // for key
+
+        int initialPermutation = permuteMSB(plaintext, P8Initial, 8);
+
+        // Generate K1 and K2
+        int p10 = permuteMSB(key, P10, 10);
+        int L = (p10 >> 5) & 0x1F, R = p10 & 0x1F;
+        L = leftShift(L,1); R = leftShift(R,1);
+        int K1 = permuteMSB((L<<5)|R, P8, 10);
+        L = leftShift(L,2); R = leftShift(R,2);
+        int K2 = permuteMSB((L<<5)|R, P8, 10);
+
+        //S-DES Process
+        int afterFK1 = fK(initialPermutation, K1);
+        int afterSW = SW(afterFK1);
+        int afterFK2 = fK(afterSW, K2);
+
+        int finalpermutation = permuteMSB(afterFK2, P8Final, 8);
+
+        return finalpermutation;
+
+    }
+
+    public static int Decrypt(int key, int ciphertext) {
+        int[] P8Initial = {2, 6, 3, 1, 4, 8, 5, 7};
+        int[] P8Final = {4, 1, 3, 5, 7, 2, 8, 6};
+        int[] P10 = {3,5,2,7,4,10,1,9,8,6};
+        int[] P8  = {6,3,7,4,8,5,10,9};
+
+        int initialPermutation = permuteMSB(ciphertext, P8Initial, 8);
+
+        int p10 = permuteMSB(key, P10, 10);
+        int L = (p10 >> 5) & 0x1F, R = p10 & 0x1F;
+        L = leftShift(L,1); R = leftShift(R,1);
+        int K1 = permuteMSB((L<<5)|R, P8, 10);
+        L = leftShift(L,2); R = leftShift(R,2);
+        int K2 = permuteMSB((L<<5)|R, P8, 10);
+
+        int afterFK1 = fK(initialPermutation, K2);
+        int afterSW = SW(afterFK1);
+        int afterFK2 = fK(afterSW, K1);
+
+        int finalpermutation = permuteMSB(afterFK2, P8Final, 8);
+
+        return finalpermutation;
+    }
+
 
     public static void main(String[] args) {
+
 
 
     }
